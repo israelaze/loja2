f09127fa401294398814cb6dbc4f1043f8434f31
Israel Fraga <israelaze@gmail.com>
2021-12-24 16:38:14 -0300

    Suporte HATEOAS

---
+++ 08- HATEOAS/src/main/java/br/com/loja/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,83 @@
+package br.com.loja.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.loja.services.exceptions.BadRequestException;
+import br.com.loja.services.exceptions.EntityNotFoundException;
+import br.com.loja.services.exceptions.ServiceException;
+import br.com.loja.services.exceptions.UnprocessableEntityException;
+
+//Captura e trata uma exceção específica no Controller 
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	//Status code 400
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<StandardError> badRequest(BadRequestException e, HttpServletRequest request){
+		
+		StandardError error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setError("ERRO");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	
+	}
+	//Status code 404
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		
+		StandardError error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError("ERRO");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	
+	}
+	//Status code 500
+	@ExceptionHandler(ServiceException.class)
+	public ResponseEntity<StandardError> exceptions(Exception e, HttpServletRequest request){
+		
+		StandardError error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		error.setError("Erro servidor");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+		
+	}
+	//Status code 422
+	@ExceptionHandler(UnprocessableEntityException.class)
+	public ResponseEntity<StandardError> unprocessableEntity(UnprocessableEntityException e, HttpServletRequest request){
+		
+		StandardError error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		error.setError("Não processado");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+	
+	}
+	
+	
+}
---
+++ 08- HATEOAS/src/main/java/br/com/loja/services/ProdutoServices.java
@@ -0,0 +1,170 @@
+package br.com.loja.services;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.stereotype.Service;
+
+import br.com.loja.controllers.ProdutoController;
+import br.com.loja.dtos.DtoToProduto;
+import br.com.loja.dtos.ProdutoGetDTO;
+import br.com.loja.dtos.ProdutoPostDTO;
+import br.com.loja.dtos.ProdutoPutDTO;
+import br.com.loja.entities.Produto;
+import br.com.loja.repositories.ProdutoRepository;
+import br.com.loja.services.exceptions.BadRequestException;
+import br.com.loja.services.exceptions.EntityNotFoundException;
+import br.com.loja.services.exceptions.UnprocessableEntityException;
+
+@Service
+public class ProdutoServices {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public String cadastrar(ProdutoPostDTO dto) {
+
+		// verificando se o produto já existe no banco
+		if (produtoRepository.findByCodigo(dto.getCodigo()) != null) {
+			throw new BadRequestException("Código já cadastrado!");
+		} else {
+
+			// transferindo os dados do Dto para o Produto
+			Produto produto = DtoToProduto.postDtoToProduto(dto);
+
+			// salvando o novo produto no banco
+			produtoRepository.save(produto);
+
+			// Respondendo ao controlador
+			String response = "Produto cadastrado com sucesso!";
+			return response;
+		}
+
+	}
+
+	public CollectionModel<ProdutoGetDTO> buscarProdutos() {
+
+		// Buscando todos os produtos no banco e armazenando numa lista de produtos
+		List<Produto> listaProduto = (List<Produto>) produtoRepository.findAll();
+
+		// Criando uma lista de ProdutosGetDTO
+		List<ProdutoGetDTO> listaGetDto = new ArrayList<ProdutoGetDTO>();
+
+		// pegando cada produto na listaProduto, transferindo um a um para o getDto 
+		for (Produto produto : listaProduto) {
+			ProdutoGetDTO getDto = new ProdutoGetDTO(produto);
+
+			// adicionando links HATEOAS para cada produto
+			getDto.add(linkTo(methodOn(ProdutoController.class).buscarPorId(produto.getIdProduto())).withSelfRel());
+			getDto.add(linkTo(methodOn(ProdutoController.class).buscarProdutos()).withRel("Lista de Produtos"));
+			
+			// adicionando na listaGetDto
+			listaGetDto.add(getDto);
+
+		}
+
+		// adicionando links HATEOAS para o recurso e respondendo ao controlador
+		return CollectionModel.of(listaGetDto).add(linkTo(methodOn(ProdutoController.class).buscarProdutos()).withSelfRel());
+	}
+
+	public ProdutoGetDTO buscarPorId(Integer idProduto) {
+
+		// Buscando um produto específico no banco
+		Optional<Produto> result = produtoRepository.findById(idProduto);
+
+		if (result == null || result.isEmpty()) {
+			throw new EntityNotFoundException("Produto não encontrado: " + idProduto);
+		} else {
+
+			// Trazendo o produto encontrado no banco
+			Produto produto = result.get();
+
+			// transferindo os dados do Produto para o GetDto
+			ProdutoGetDTO getDto = new ProdutoGetDTO(produto);
+
+			// adicionando links HATEOAS
+			getDto.add(linkTo(methodOn(ProdutoController.class).buscarProdutos()).withRel("Lista de Produtos"));
+			getDto.add(linkTo(methodOn(ProdutoController.class).buscarPorId(produto.getIdProduto())).withSelfRel());
+
+			// Respondendo ao controlador
+			return getDto;
+		}
+
+	}
+
+	public ProdutoGetDTO buscarPorCodigo(String codigo) {
+
+		// Buscando um produto específico no banco
+		Produto produto = produtoRepository.findByCodigo(codigo);
+
+		if (produto != null) {
+
+			// transferindo os dados do Produto para o GetDto
+			ProdutoGetDTO getDto = new ProdutoGetDTO(produto);
+
+			// adicionando links HATEOAS
+			getDto.add(linkTo(methodOn(ProdutoController.class).buscarProdutos()).withRel("Lista de Produtos"));
+			getDto.add(linkTo(methodOn(ProdutoController.class).buscarPorCodigo(produto.getCodigo())).withSelfRel());
+
+			// Respondendo ao controlador
+			return getDto;
+
+		} else {
+			throw new EntityNotFoundException("Produto não encontrado: " + codigo);
+		}
+
+	}
+
+	public String atualizar(ProdutoPutDTO dto) {
+
+		// Buscando um produto específico no banco
+		Optional<Produto> result = produtoRepository.findById(dto.getIdProduto());
+
+		if (result == null || result.isEmpty()) {
+			throw new UnprocessableEntityException("Produto não encotrado: " + dto.getIdProduto());
+		} else {
+
+			// Trazendo o produto encontrado no banco
+			Produto produto = result.get();
+
+			// transferindo os dados do dto para o produto e salvando alterações
+			DtoToProduto.putDtoToProduto(produto, dto);
+			produtoRepository.save(produto);
+
+			// Respondendo ao controlador
+			String response = "Produto atualizado com sucesso!";
+			return response;
+		}
+
+	}
+
+	public String excluir(Integer idProduto) {
+
+		// Buscando um produto específico no banco
+		Optional<Produto> result = produtoRepository.findById(idProduto);
+
+		if (result == null || result.isEmpty()) {
+			throw new UnprocessableEntityException("Produto não encontrado!");
+		} else {
+
+			// Trazendo o produto encontrado no banco
+			Produto produto = result.get();
+
+			// Excluindo o produto
+			produtoRepository.delete(produto);
+
+			// Respondendo ao controlador
+			String response = "Produto excluído com sucesso!";
+			return response;
+		}
+
+	}
+
+}
