d4f62f6c5d635b14579c834488581a77a5318a70
Israel Fraga <israelaze@gmail.com>
2021-12-30 17:40:47 -0300

    Integração contínua com Travis CI

---
+++ 11- Travis CI/Suporte Docker/src/main/java/br/com/loja/dtos/ProdutoPostDTO.java
@@ -0,0 +1,19 @@
+package br.com.loja.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoPostDTO {
+
+	private String marca;
+	private String nome;
+	private String codigo;
+	private String categoria;
+	private Double estoque;
+	private Double preco;
+	private String dataFabricacao;
+	private String dataVencimento;
+
+}
