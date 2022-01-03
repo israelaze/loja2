3d1f023046d11177721bdcb7aeaca55fe6a3776b
Israel Fraga <84162152+israelaze@users.noreply.github.com>
2022-01-03 16:17:07 -0300

    Delete 12- AWS part1 directory

--- 12- AWS part1/Suporte Docker/src/main/java/br/com/loja/services/exceptions/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package br.com.loja.services.exceptions;
-
-public class BadRequestException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-	public BadRequestException(String msg) {
-		super(msg);
-	}
-}
